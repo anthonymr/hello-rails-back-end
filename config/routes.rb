986c19111d75ecf68cbe34028fb6144a6653066a
Anthony Martin <anthonymartin20@gmail.com>
2023-06-05 14:37:44 -0400

    Create route for random action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  scope '/api' do
+    scope '/v1' do
+      scope '/messages' do
+        get '/random', to: 'messages#random'
+      end
+    end
+  end
 end
